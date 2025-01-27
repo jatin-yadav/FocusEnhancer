7785bacd415565029e80c3a6862bfcfcd6b393cf
jatin-yadav <jatinyadav19oct@gmail.com>
2025-01-28 00:07:37 +0530

    Refactor Home component layout and remove unused SVG import for cleaner code

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,34 +2,28 @@ import CuteEggShapedLamp from '../../public/assets/Cute_Egg-Shaped_Lamp.png'
 import { BoldButton } from '../components/BoldButton'
 import NavBar from '../components/NavBar'
 import { RightArrow } from '../icons/RightArrow'
-import eclipsbg from '../../src/assets/eclipse_hero_background.svg'
 
 const Home = () => {
     return (
-        <>
-            <section className="bg-[#331f76] h-screen">
-                <div className='fixed'>
-                    <NavBar />
-                </div>
-                <div className="flex">
-                    <div className='w-1/2 flex justify-center items-center'>
-                        <div className='px-5'>
-                            <h1 className='text-5xl font-bold text-white'>Focus Enhancer:<br /> Unlock Your Brain's Potential</h1>
-                            <h3 className='text-xl py-2 text-gray-300 capitalize'>Break barriers and elevate your mind <br />your companion in unlocking focus and limitless cognitive growth</h3>
-                            <div className='py-4'>
-                                <BoldButton text='Play For Free' variant='primary' endIcon={<RightArrow />} />
-                            </div>
+        <section className="bg-[#331f76] h-screen">
+            <div className='fixed'>
+                <NavBar />
+            </div>
+            <div className="flex">
+                <div className='w-1/2 flex justify-center items-center'>
+                    <div className='px-5'>
+                        <h1 className='text-5xl font-bold text-white'>Focus Enhancer:<br /> Unlock Your Brain's Potential</h1>
+                        <h3 className='text-xl py-2 text-gray-300 capitalize'>Break barriers and elevate your mind <br />your companion in unlocking focus and limitless cognitive growth</h3>
+                        <div className='py-4'>
+                            <BoldButton text='Play For Free' variant='primary' endIcon={<RightArrow />} />
                         </div>
                     </div>
-                    <div className='w-1/2 h-screen'>
-                        <img className='w-full h-full' src={CuteEggShapedLamp} alt="CuteEggShapedLamp" />
-                    </div>
                 </div>
-            </section>
-            <section className='absolute'>
-                <img className='relative' height="476px" src={eclipsbg} alt="" />
-            </section>
-        </>
+                <div className='w-1/2 h-screen'>
+                    <img className='w-full h-full' src={CuteEggShapedLamp} alt="CuteEggShapedLamp" />
+                </div>
+            </div>
+        </section>
     )
 }
 
